55f7e317a208e1e11404b5ce1c107d9cdea252cc
Matthieu <matthieu.felten22@gmail.com>
2024-08-31 20:59:37 +0200

    Update

--- popup/popup.js
+++ popup/popup.js
@@ -4,16 +4,9 @@ const email = document.getElementById("email");
 const mailOpen = document.getElementById("mailOpen")
 const state = document.getElementById("state")
 
-submit.addEventListener("click", save);
-textInput.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    save();
-  }
-});
-
-function save() {
+textInput.oninput = (event) => {
+  email.innerText = `Email : send-in@${textInput.value}.softgarden.io`;
   chrome.storage.local.set({ companyName: textInput.value });
-  reload();
 }
 
 mailOpen.addEventListener("click", () => {
@@ -24,17 +17,13 @@ state.addEventListener("click", () => {
   chrome.storage.local.set({ disabled: state.checked });
 })
 
-reload();
-
-function reload() {
-  chrome.storage.local.get(["companyName"], (result) => {
-    textInput.value = result.companyName
-    email.innerText = `Email : send-in@${result.companyName}.softgarden.io`;
-  });
-  chrome.storage.local.get(["useGmail"], (result) => {
-    mailOpen.checked = result.useGmail
-  });
-  chrome.storage.local.get(["disabled"], (result) => {
-    state.checked = result.disabled
-  });
-}
+chrome.storage.local.get(["companyName"], (result) => {
+  textInput.value = result.companyName
+  email.innerText = `Email : send-in@${textInput.value}.softgarden.io`;
+});
+chrome.storage.local.get(["useGmail"], (result) => {
+  mailOpen.checked = result.useGmail
+});
+chrome.storage.local.get(["disabled"], (result) => {
+  state.checked = result.disabled
+});
